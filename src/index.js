f276dd0f9ed859ec6648807f31bfa2d9f80d24e5
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-05-19 17:56:45 -0400

    Fetch the data and pass it to createChoroplethMap
    - Fetch county data and parse JSON
    - Fetch education data and parse JSON
    - Pass parsed data to createChoroplethMap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import createChoroplethMap from "./app/app";
 import "./index.scss";
 
 function ready() {
-  createChoroplethMap();
+  Promise.all([
+    fetch(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+    ).then((usResponse) => usResponse.json()),
+    fetch(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
+    ).then((educationResponse) => educationResponse.json()),
+  ])
+    .then(([usData, educationData]) => {
+      createChoroplethMap(usData, educationData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 if (document.readyState === "loading") {
