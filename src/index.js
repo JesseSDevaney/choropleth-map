9f4d69d5d67579ad9c509f52165aa406d742a03e
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2021-05-19 17:42:45 -0400

    Create svg and append to the root node
    - Install and import d3 (package.json, app.js)
    - Create svg and append to root node (app.js)
    - Style svg (app.scss)
    - Center svg in the middle of the page (index.scss)
    - Import app and call the main function (index.js)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import createChoroplethMap from "./app/app";
 import "./index.scss";
+
+function ready() {
+  createChoroplethMap();
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
